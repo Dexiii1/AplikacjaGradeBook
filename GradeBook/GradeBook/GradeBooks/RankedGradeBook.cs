bdc5eb8bb31bdfae32cc90745f3d628af2dcb7b0
Dexiii1 <118063665+Dexiii1@users.noreply.github.com>
2024-04-15 22:47:43 +0200

    Add files via upload

---
+++ GradeBook/GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            int top20 = (int)Math.Ceiling(0.2 * Students.Count);
+            List<double> oceny = new List<double>();
+
+            List<Student> _Students = new List<Student>(Students);
+            _Students.Sort((y, x) => x.AverageGrade.CompareTo(y.AverageGrade));
+
+            foreach (Student ocena in _Students)
+            {
+                oceny.Add(ocena.AverageGrade);
+            }
+
+            if (oceny[top20] < averageGrade) return 'A';
+            else if (oceny[(2 * top20)] < averageGrade) return 'B';
+            else if (oceny[(3 * top20)] < averageGrade) return 'C';
+            else if (oceny[(4 * top20)] < averageGrade) return 'D';
+            else return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStudentStatistics(name);
+        }
+    }
+}
+
